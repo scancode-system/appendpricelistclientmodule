8703c0e8d15d6a63a07e488a4404b574d59c7124
Your Name <you@example.com>
2020-07-30 15:56:28 -0300

    .

---
+++ Services/InfoService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\AppendPriceListClient\Services; 
+
+
+use Modules\Portal\Services\Validation\Data\InfoValidationService;
+
+
+class InfoService extends InfoValidationService
+{
+
+
+	public function rule($data, $index, $columns)
+	{
+		return [
+			'price_list_name' =>  'string|max:255'
+		];
+	}
+
+	public function modifiers()
+	{
+		return  [];
+	}
+
+	public function columnsFormat()
+	{
+		return  [];
+	}
+
+}
